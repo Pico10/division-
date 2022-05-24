4046c5630286358d40ad2e9ede7dc1cf90b4d8d5
pico10 <maximovillalba2016@gmail.com>
2022-05-24 10:15:21 -0300

    se agrega nuevo proyecto

---
+++ Nueva carpeta/division.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, division;
+            try
+            {
+                Console.Write("ingrese su primer numero");
+                num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("ingrese su segundo numero");
+                num2 = Convert.ToDouble(Console.ReadLine());
+
+                division = num1 / num2;
+
+                if (num2 == 0)
+                {
+                    Console.WriteLine("no se puede dividir por 0");
+                }
+                else
+                {
+                    Console.WriteLine("el resultado de la division es {0}", division);
+                }
+
+            }
+            catch(Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+            
+
+        }
+    }
+}
